15abbc4a7dfc3ebf61e5e9b3e479d0fa60ce12ed
StevenBrennan01 <steveb1701@outlook.com>
2026-04-21 14:08:05 +0100

    Updated HUD and added Emissive ShaderGraph Material

--- Assets/_Scripts/GameManagers/LevelStartHeadPlacement.cs
+++ Assets/_Scripts/GameManagers/LevelStartHeadPlacement.cs
@@ -8,14 +8,13 @@ public class LevelStartHeadPlacement : MonoBehaviour
     private void Awake()
     {
         if (playerState == null) playerState = FindFirstObjectByType<PlayerStateController>();
-        if (startVolume == null)
-        {
-            Debug.LogWarning("Second Person start volume not found");
-        }
     }
 
     void Start()
     {
-        playerState.PlaceHeadOnStart(startVolume);
+        if (startVolume != null)
+        {
+            playerState.PlaceHeadOnStart(startVolume);
+        }
     }
 }
